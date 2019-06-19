ff37957ba2da82d59835fc4113f1e593ab45c1b1
GytisGrigonis <grigonis0@gmail.com>
2019-06-19 12:40:39 +0300

    game fuctionality added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 function shuffle(a) {
   var j, x, i;
   for (i = a.length - 1; i > 0; i--) {
@@ -13,12 +12,12 @@ function shuffle(a) {
 var num = 1;
 var cards = [];
 var i;
+const backPic = "img/purple_back.png";
 
 class Card {
-  constructor(number, frontPic, backPic) {
+  constructor(number, frontPic) {
     this.number = num;
     this.frontPic = frontPic;
-    this.backPic = backPic;
     this.flipped = false;
   }
 
@@ -34,21 +33,53 @@ var frontPics = [
   "img/card6.png"
 ];
 
-cards.push(new Card(num, frontPics[0], frontPics[0]));
+cards.push(new Card(num, frontPics[0]));
 
 for(i = 1; i <= 12; i++){
-  cards.push(new Card(num, frontPics[num], frontPics[0]));
+  cards.push(new Card(num, frontPics[num]));
   if(i%2 == 0) num++;
 }
 
 
-//cards = shuffle(cards);
+cards = shuffle(cards);
+var firstCard, secondCard;
+var disabledBoard = false;
+var oneFlippedCard = false;
+
+function resetBoard(){
+  firstCard = null;
+  secondCard = null;
+  disabledBoard = false;
+  oneFlippedCard = false;
+}
 
 for(i = 1; i <= 12; i++){
   var cardTag = document.getElementById(i);
   cardTag.onclick = function(e) {
-    img = window.event.srcElement;
+    if(disabledBoard) return;
+    if(cards[this.id].flipped) return;
+    if(!oneFlippedCard){
+      oneFlippedCard = true;
+      firstCard = this;
+      firstCard.src = cards[firstCard.id].frontPic;
+      return;
+    }
 
-    img.src = cards[img.id].frontPic;
+    secondCard = this;
+    secondCard.src = cards[secondCard.id].frontPic;
+
+    if(cards[firstCard.id].number == cards[secondCard.id].number){
+      cards[firstCard.id].flipped = true;
+      cards[secondCard.id].flipped = true;
+      resetBoard();
+    }
+    else{
+      disabledBoard = true;
+      setTimeout(() => {
+        firstCard.src = backPic;
+        secondCard.src = backPic;
+        resetBoard();
+      }, 1500);
+    }
   }
 }
